3ac81ab52d32843053e4e86345089611ae4c41f2
Ozeki Masaki <github.com.boneless110@passmail.net>
2025-03-22 09:29:00 +0900

    starlightLlmsTxtの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import rehypePrettyCode from 'rehype-pretty-code';
 import { siteConfig } from './src/config';
 
 import sitemap from '@astrojs/sitemap';
+import starlightLlmsTxt from 'starlight-llms-txt'
 
 export default defineConfig({
   site: siteConfig.site,
   base: 'AIAU_Blog',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), starlightLlmsTxt()],
   markdown: {
     rehypePlugins: [
       [rehypePrettyCode, {
